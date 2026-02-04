4d1cfa5e2670fc32bc15b96bcb5fe86b82412639
namnkahn1607 <nlnkandthebois@gmail.com>
2026-02-04 10:18:40 +0700

    Edge

--- data_structures/graph/edge.go
+++ data_structures/graph/edge.go
@@ -11,6 +11,15 @@ func NewEdge(u, v, weight int) *Edge {
 	return &Edge{u, v, weight}
 }
 
+/*
+Arbitrary endpoint for Undirected Edge;
+Source endpoint for Directed Edge.
+*/
+func (e *Edge) Head() int {
+	return e.v
+}
+
+/* Another endpoint of a vertex in an edge. */
 func (e *Edge) Other(v int) int {
 	switch v {
 	case e.v:
@@ -22,6 +31,7 @@ func (e *Edge) Other(v int) int {
 	panic("invalid edge endpoint")
 }
 
+/* An edge's weight. */
 func (e *Edge) Weight() int {
 	return e.weight
 }
