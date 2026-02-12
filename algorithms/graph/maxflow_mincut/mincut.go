30ccd3bea6a5bf5c2aebe6e92b6ac0597659b1f8
namnkahn1607 <nlnkandthebois@gmail.com>
2026-02-12 22:26:11 +0700

    Maxflow Mincut

---
+++ algorithms/graph/maxflow_mincut/mincut.go
@@ -0,0 +1,15 @@
+/* API: Maxflow */
+
+package maxflow
+
+import (
+	graph "azure/data_structures/flow_network"
+	"math"
+)
+
+const INF = math.MaxInt64
+
+type MinCut struct {
+	Edges    []*graph.FlowEdge
+	Capacity int
+}
