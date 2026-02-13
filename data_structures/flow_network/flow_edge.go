0f181915310c41c54b047251de34dae029db8e36
namnkahn1607 <nlnkandthebois@gmail.com>
2026-02-13 08:21:06 +0700

    Flow Edge

--- data_structures/flow_network/flow_edge.go
+++ data_structures/flow_network/flow_edge.go
@@ -1,27 +1,43 @@
-/* Data Structure: Flow Edge */
+/* Data Structure: Directed Flow Edge */
 
 package graph
 
 type FlowEdge struct {
-	v, w      int
+	from, to  int
 	flow, cap int
 }
 
-func NewFlowEdge(from, to, cap int) *FlowEdge {
-	return &FlowEdge{
-		v:   from,
-		w:   to,
-		cap: cap,
-	}
+/* Create a new hollow Directed Flow Edge. */
+func NewFlowEdge(v, w, cap int) *FlowEdge {
+	return &FlowEdge{v, w, 0, cap}
 }
 
-/* Add an amount of residual value to one endpoint of a flow edge. */
+/* The head endpoint of the Flow Edge. */
+func (e *FlowEdge) Head() int {
+	return e.from
+}
+
+/* Another endpoint of a vertex in a Flow Edge. */
+func (e *FlowEdge) Other(v int) int {
+	switch v {
+	case e.from:
+		return e.to
+	case e.to:
+		return e.from
+	}
+
+	panic("invalid edge endpoint")
+}
+
+/* Add an amount of residual to a Flow Edge. */
 func (e *FlowEdge) AddResidualTo(v int, delta int) {
 	switch v {
-	case e.v:
+	case e.from:
 		e.flow -= delta
-	case e.w:
+	case e.to:
 		e.flow += delta
+	default:
+		panic("invalid edge endpoint")
 	}
 
 	if e.flow < 0 || e.flow > e.cap {
@@ -29,25 +45,13 @@ func (e *FlowEdge) AddResidualTo(v int, delta int) {
 	}
 }
 
-/* Residual value to one endpoint of a flow edge. */
+/* Residual value corresponding to a vertex in a Flow Edge. */
 func (e *FlowEdge) ResidualTo(v int) int {
 	switch v {
-	case e.v:
-		return e.flow
-	case e.w:
-		return e.cap - e.flow
-	}
-
-	panic("invalid edge endpoint")
-}
-
-/* Another endpoint of a vertex in a flow edge. */
-func (e *FlowEdge) Other(v int) int {
-	switch v {
-	case e.v:
-		return e.w
-	case e.w:
-		return e.v
+	case e.from:
+		return e.to
+	case e.to:
+		return e.from
 	}
 
 	panic("invalid edge endpoint")
